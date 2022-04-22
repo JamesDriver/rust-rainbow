3654dd33b16be229cc4dd961893027038e01b23a
James Driver <jamesdriver1999@gmail.com>
2022-04-21 21:35:31 -0400

    rayon thread pooling test added

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::{
     sync::Mutex,
 };
 use std::sync::Arc;
+use rayon;
 
 mod lib;
 use crate::lib::{RunTiming, ThreadPool};
@@ -15,6 +16,32 @@ const MAX_WORKER: usize = 4;
 
 fn hash_me(line: String) -> String {
     hex::encode(sha1::Sha1::digest(line.as_bytes())) + "," + &line + ","
+}
+
+pub fn better_thread_pooling(file_name: &str) -> Result<RunTiming, Box<dyn Error>> {
+    let mut timing: RunTiming = RunTiming::default();
+    let pool = rayon::ThreadPoolBuilder::new()
+    .num_threads(8)
+    .build()
+    .unwrap();
+    let wordlist_file = &File::open(file_name)?;
+    let reader = BufReader::new(wordlist_file);
+    timing.set_init();
+    //let hashes = Vec::new();
+    let file = File::create("/usr/local/share/wordlists/SecLists/Passwords/darkweb2017-top10000-test4.txt")?;
+    let mut stream = BufWriter::with_capacity(10000,file);
+    //let data = Arc::new(Mutex::new(stream));
+    pool.install(|| {
+        for line in reader.lines() {
+            let line = match line {
+                Ok(l) => l,
+                Err(_) => continue,
+            };
+            write!(stream, "{}", (hex::encode(sha1::Sha1::digest(line.as_bytes())) + "," + &line + ","));
+        }
+    });
+    timing.set_complete();
+    Ok(timing)
 }
 
 pub fn thread_pooling(file_name: &str) -> Result<RunTiming, Box<dyn Error>> {
@@ -105,8 +132,8 @@ pub fn single_threaded(file_name: &str) -> Result<RunTiming, Box<dyn Error>> {
 }
 
 fn percent_faster(a: RunTiming, b: RunTiming) {
-    let ac = a.completion;
-    let bc = b.completion;
+    let ac = a.completion-a.initialization;
+    let bc = b.completion-b.initialization;
     println!("Option 2 is {} times faster than option 1: {}, {}", (ac/bc), ac, bc)
 }
 
@@ -120,6 +147,7 @@ fn main() {
     let base_single_timing = single_threaded("/usr/local/share/wordlists/SecLists/Passwords/darkweb2017-top10000.txt");
     let base_multi_timing = multi_threaded("/usr/local/share/wordlists/SecLists/Passwords/darkweb2017-top10000.txt");
     let thread_pooling_timing = thread_pooling("/usr/local/share/wordlists/SecLists/Passwords/darkweb2017-top10000.txt");
+    let better_thread_pooling = better_thread_pooling("/usr/local/share/wordlists/SecLists/Passwords/darkweb2017-top10000.txt");
     let a = match base_single_timing {
         Ok(t) => t,
         Err(_) => panic!("err"),
@@ -132,6 +160,10 @@ fn main() {
         Ok(t) => t,
         Err(_) => panic!("err"),
     };
+    let _d = match better_thread_pooling {
+        Ok(t) => t,
+        Err(_) => panic!("err"),
+    };
     //percent_faster(a, b);
-    percent_faster(a, c);
+    percent_faster(a, _d);
 }
